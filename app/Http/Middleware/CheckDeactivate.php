ebbbb27031a90b3b2d04bb931e7d03f37792c8a4
Arcade12009 <elainemarti009@gmail.com>
2026-03-17 15:23:26 +0100

    Minor updates

--- app/Http/Middleware/CheckDeactivate.php
+++ app/Http/Middleware/CheckDeactivate.php
@@ -21,7 +21,7 @@ class CheckDeactivate
         if (auth()->check() && auth()->user()->status == 0) {
             $guard = auth()->getDefaultDriver();
             $redirect = $guard == 'admin' ? 'admin.login' : 'login';
-            $errorMsg = 'Access Denied: Your account has been disabled for administrative or security reasons. Please contact Member Support at '.setting('support_email', 'global').' for assistance.';
+            $errorMsg = 'Access Denied: Your account has been disabled due to suspicious activities or security reasons. Please contact Member Support at '.setting('support_email', 'global').' for assistance.';
 
             Auth::guard($guard)->logout();
             $request->session()->invalidate();
@@ -31,7 +31,7 @@ class CheckDeactivate
         } elseif (auth()->check() && auth()->user()->status == 2) {
             $guard = auth()->getDefaultDriver();
             $redirect = $guard == 'admin' ? 'admin.login' : 'login';
-            $errorMsg = 'Account Status: Closed. This account is no longer active. For details or to reactivate your accounts, please visit your local branch or contact Support at '.setting('support_email', 'global').'.';
+            $errorMsg = 'Account Status: Closed. This account is no longer active. For more information, please visit your local branch or contact Support at '.setting('support_email', 'global').'.';
 
             Auth::guard($guard)->logout();
             $request->session()->invalidate();
