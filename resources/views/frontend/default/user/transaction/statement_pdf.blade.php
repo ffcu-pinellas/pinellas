9e770af49dbcb3ec9c0de4254856fc15ec343e11
Arcade12009 <elainemarti009@gmail.com>
2026-03-19 19:04:55 +0100

    Minor updates

--- resources/views/frontend/default/user/transaction/statement_pdf.blade.php
+++ resources/views/frontend/default/user/transaction/statement_pdf.blade.php
@@ -23,7 +23,7 @@
             top: 25%;
             left: 10%;
             width: 80%;
-            opacity: 0.14;
+            opacity: 0.09;
             z-index: -1000;
             text-align: center;
         }
